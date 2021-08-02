a82a0769ce655ea83c88aa90af9d9b806aa88e05
Jacky Cheng <j.cheng0915@gmail.com>
2021-08-01 22:18:51 -0400

    Including code.
    Assets seem to have been added from inside the Unreal Engine 4 IDE.

---
+++ README.md
@@ -0,0 +1,3 @@
+Learning the basics of Unreal Engine 4.
+
+This is a simple project for "throwing" once until the projectile is picked up.
---
+++ Source/Pickup/PickupGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PickupGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APickupGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APickupGameMode();
+};
+
+
+
